60dac751f004ef58a06bf3141cb142bdf093d53e
jagoum <jagoum24@gmail.com>
2025-02-18 17:03:16 +0100

    Fixed all bugs and and optimized the code

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+mod reverse_array;
+mod get_values;
+use get_values::{get_array, get_nums};
+use reverse_array::{add_element, remove_index, reverse_array, SIZE};
+
+fn main() {
+    println!("\nPlease enter the number element of the array [1 <= number of elements <= 50]");
+    let num_of_elem = get_nums();
+    let arr= get_array(num_of_elem);
+
+    println!("Please Enter\n1=>To reverse the array\n2=>To remove an element from the array\n3To add an element into the array");
+    reverse_array(num_of_elem, arr);
+    println!("\nPlease enter the index at which you would like to remove an element from the initail array");
+    let index =get_nums();
+    remove_index(index, arr, num_of_elem);
+
+
+    println!("\nPlease enter the index at which you would like to insert an element");
+    let index = get_nums();
+    println!("\nEnter the element you would like to insert");
+    let element = get_nums();
+    add_element(index, element, num_of_elem, arr);
+
+    
+}
+
