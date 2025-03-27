742dd23570a31a4d4b518d23eba9aa63acd029a5
jagoum <jagoum24@gmail.com>
2025-03-27 16:32:58 +0100

    could populate table

--- async_web_server/src/main.rs
+++ async_web_server/src/main.rs
@@ -4,18 +4,18 @@ use scraper::{Html, Selector};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn error::Error>> {
-    // this is the url of the website we are fetching the informations from
-    let url = "https://rapidapi.com/blog/most-popular-api/";
+    // this is the url of the website we are fetching the information from
+    let url = "https://www.cloudflare.com/plans/";
     let response = reqwest::get(url).await?; //this  gets the api response from the web server with the all information that is to say it gets the html content from the website
     let body = response.text().await?; // this is to extract the body of the response
     let html = Html::parse_document(&body);
 
-    let selector = Selector::parse("img").expect("Failed to parse selector");
+    let selector = Selector::parse("a").expect("Failed to parse selector");
 
     for element in html.select(&selector) {
-        if let Some(val) = element.attr("a") {
+        if let Some(val) = element.attr("href") {
             //this one checks if the attribute href is also present in the link is found
-            println!("Found Image: {val}")
+            println!("{val}")
         }
     }
 
