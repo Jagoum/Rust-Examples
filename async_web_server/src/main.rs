2b9aade0ab9068e3509c4e28c8b5b9c563117b00
jagoum <jagoum24@gmail.com>
2025-04-01 12:39:58 +0100

    chore: commiting latest changes

--- async_web_server/src/main.rs
+++ async_web_server/src/main.rs
@@ -10,10 +10,10 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
     let body = response.text().await?; // this is to extract the body of the response
     let html = Html::parse_document(&body);
 
-    let selector = Selector::parse("a").expect("Failed to parse selector");
+    let selector = Selector::parse("img").expect("Failed to parse selector");
 
     for element in html.select(&selector) {
-        if let Some(val) = element.attr("href") {
+        if let Some(val) = element.attr("src") {
             //this one checks if the attribute href is also present in the link is found
             println!("{val}")
         }
